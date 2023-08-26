cac09d1b10be17727a870447ccbfbad291cecfb0
Frankz Alarcon <leninalarcon7@gmail.com>
2023-08-26 15:23:13 -0500

    feat(bookcollections/models): models creation without logic

--- bookcollections/models.py
+++ bookcollections/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from articles.models import Article
 
 # Create your models here.
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    is_public = models.BooleanField(default=False, null=False)
+
+    category = models.CharField(
+        max_length=100,
+        choices=Article.Category.choices,
+        default=Article.Category.UNKNOWN
+    )
+
+    def __str__(self):
+        return self.name
