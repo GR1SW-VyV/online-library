05e9cafdb936f76c898793ad1053794b62c6bca4
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2023-08-29 06:59:31 -0500

    feat: Add Note model

--- annotations/models.py
+++ annotations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Note(models.Model):
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    is_important = models.BooleanField(default=False)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #document = models.ForeignKey(Document, on_delete=models.CASCADE)
