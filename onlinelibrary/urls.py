9bd089e7e71d254244087781a1229f00e775d91d
Marcela Montalvo <marcelamontalvo@Marcelas-MacBook-Pro.local>
2023-08-30 00:19:58 -0500

    chore(bookcollectionsviews.py): remove unused method imports
    Removes unused method imports in 'views.py' view_base.

--- onlinelibrary/urls.py
+++ onlinelibrary/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bookcollections.views import vista_preview , view_base , view_collections
+from bookcollections.views import  view_collections
 
 urlpatterns = [
     path('admin/', admin.site.urls),
