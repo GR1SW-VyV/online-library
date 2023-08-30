b5471c1f18580e0db5e1cbdbd88775c9728a8856
Marcela Montalvo <marcelamontalvo@Marcelas-MacBook-Pro.local>
2023-08-29 21:24:54 -0500

    feat:(bookscollections/views) add view_collections to views.py      Adds the view_collections function in views.py to render conllections.html

--- bookcollections/views.py
+++ bookcollections/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from bookcollections.models import CollectionDAO
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+#return base.html
+def view_base(request):
+   return render(request, './base.html')
+
+#return collections.html
+def view_collections(request):
+      return render(request, './collections.html')
+
