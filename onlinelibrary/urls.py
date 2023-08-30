75d30442aea732b5236d85c35a79be44744b5bed
Frankz Alarcon <leninalarcon7@gmail.com>
2023-08-30 07:13:44 -0500

    refactor: merge

--- onlinelibrary/urls.py
+++ onlinelibrary/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bookcollections.views import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', view, name='view'),
 ]
