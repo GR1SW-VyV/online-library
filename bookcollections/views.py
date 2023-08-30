5454974d08498cfc01b4c972d9195141b4d38144
Marcela Montalvo <marcelamontalvo@Marcelas-MacBook-Pro.local>
2023-08-29 21:39:40 -0500

    feat(views): add collections list for testing
    Adds a collections list for testing purposes in views.py.

--- bookcollections/views.py
+++ bookcollections/views.py
@@ -12,3 +12,58 @@ def view_base(request):
 def view_collections(request):
       return render(request, './collections.html')
 
+#create collections data for test 
+
+collections = [
+   {
+      'id': 1,
+      'name': 'collection1',
+      'description': 'description1',
+      'categories': ['Categoría A', 'Categoría B'],
+      'books': [
+            {
+               'title': 'Libro 1',
+               'author': 'Autor 1',
+               
+            },
+            {
+               'title': 'Libro 2',
+               'author': 'Autor 2',
+            },
+         ]
+   },
+   {
+      'id': 2,
+      'name': 'collection2',
+      'description': 'description2',
+      'categories': ['Categoría C', 'Categoría D'],
+      'books': [
+         {
+               'title': 'Libro 1',
+               'author': 'Autor 1',
+               
+            },
+      ]
+   },
+   {
+      'id': 3,
+      'name': 'collection3',
+      'description': 'description3',
+      'categories': ['Categoría F', 'Categoría E'],
+      'books': [
+         {
+               'title': 'Libro 1',
+               'author': 'Autor 1',
+            },
+         {
+               'title': 'Libro 7',
+               'author': 'Autor 7',
+            },
+         {
+               'title': 'Libro 5',
+               'author': 'Autor 5',
+            },
+      ]
+   },
+]
+
