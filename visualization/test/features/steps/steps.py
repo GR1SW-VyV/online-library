07af1624fd2812222c248ca284cf180f2fbcb76e
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-08-30 07:37:02 -0500

    test(visualization): auto generated step definitions for refined feature

--- visualization/test/features/steps/steps.py
+++ visualization/test/features/steps/steps.py
@@ -3,96 +3,75 @@ from behave import *
 use_step_matcher("re")
 
 
-@given("the book Clean Code")
+@given("I have the book (?P<book>.+) with the id (?P<book_id>.+)")
+def step_impl(context, book, book_id):
+    """
+    :type context: behave.runner.Context
+    :type book: str
+    :type book_id: str
+    """
+    raise NotImplementedError(u'STEP: Given I have the book <book> with the id <book_id>')
+
+
+@step("I am logged in with my username (?P<username>.+)")
+def step_impl(context, username):
+    """
+    :type context: behave.runner.Context
+    :type username: str
+    """
+    raise NotImplementedError(u'STEP: And I am logged in with my username <username>')
+
+
+@when("I am reading the details of the book")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Given the book Clean Code')
+    raise NotImplementedError(u'STEP: When I am reading the details of the book')
 
 
-@when("I am reviewing the details of the book")
-def step_impl(context):
+@then("it should display my personal general annotations (?P<my_general_annotations>.+)")
+def step_impl(context, my_general_annotations):
     """
     :type context: behave.runner.Context
+    :type my_general_annotations: str
     """
-    raise NotImplementedError(u'STEP: When I am reviewing the details of the book')
+    raise NotImplementedError(u'STEP: Then it should display my personal general annotations <my_general_annotations>')
 
 
-@then("it should display the following reviews:")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Then it should display the following reviews:
-    | Marcela Tapia | 21 - 03 - 2023 | A must - read for programmers! |
-    | Bryan Montalvo | 07-04-2023 | Great insights and examples.|
-    | Adamaris Diaz | 14-05-2023 | Practical advice for coding.|
-    | Mateo Gavilanez | 15-06-2023 | Well-organized and helpful.|
-    | Cristopher Perez | 04-07-2023 | Changed how I write code.| ')
-
-
-@when("I am reading the page {page_number}")
-def step_impl(context):
+@step("the general annotattions (?P<general_annotations>.+) from other users ordered by their number of followers\.")
+def step_impl(context, general_annotations):
     """
     :type context: behave.runner.Context
+    :type general_annotations: str
+    """
+    raise NotImplementedError(
+        u'STEP: And the general annotattions <general_annotations> from other users ordered by their number of followers.')
+
+
+@when("I am reading the page (?P<page_number>.+)")
+def step_impl(context, page_number):
+    """
+    :type context: behave.runner.Context
+    :type page_number: str
     """
     raise NotImplementedError(u'STEP: When I am reading the page <page_number>')
 
 
-@when("I am reading the page <page_number>")
-def step_impl(context):
+@then("it should display my personal annotations (?P<my_annotations>.+)")
+def step_impl(context, my_annotations):
     """
     :type context: behave.runner.Context
+    :type my_annotations: str
     """
-    raise NotImplementedError(u'STEP: When I am reading the page <page_number>')
+    raise NotImplementedError(u'STEP: Then it should display my personal annotations <my_annotations>')
 
 
-@then("it should display the following notes:")
-def step_impl(context):
+@step("the annotations (?P<annotations>.+) from other users ordered by their number of followers\.")
+def step_impl(context, annotations):
     """
     :type context: behave.runner.Context
+    :type annotations: str
     """
-    raise NotImplementedError(u'STEP: Then it should display the following notes:
-                              | user | page_number | date | is_important | content |
-                              | Marcela
-    Tapia | 1 | 21 - 03 - 2023 | True | Important
-    introduction
-    points. |
-    | Bryan
-    Montalvo | 1 | 07 - 04 - 2023 | False | Interesting
-    examples
-    here. |
-    | Adamaris
-    Diaz | 1 | 14 - 05 - 2023 | True | Don\'t forget these concepts.   |
-                                           | Mateo
-    Gavilanez | 1 | 15 - 06 - 2023 | False | Helpful
-    insights
-    on
-    page
-    1. |
-    | Cristopher
-    Perez | 1 | 04 - 07 - 2023 | False | Noted
-    down
-    a
-    few
-    tips. |
-    | Marcela
-    Tapia | 2 | 21 - 03 - 2023 | False | Explained
-    well
-    on
-    page
-    2. |
-    | Bryan
-    Montalvo | 2 | 07 - 04 - 2023 | True | Important
-    details
-    here. |
-    | Adamaris
-    Diaz | 2 | 14 - 05 - 2023 | False | Skipped
-    this
-    part. |
-    | Mateo
-    Gavilanez | 2 | 15 - 06 - 2023 | False | Not
-    relevant
-    for me. |
-        | Cristopher Perez | 2 | 04 - 07 - 2023 | True | Remember for future projects.| ')
+    raise NotImplementedError(
+        u'STEP: And the annotations <annotations> from other users ordered by their number of followers.')
