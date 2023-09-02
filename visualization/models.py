0cb34e0bca6c37d2379d18f3d8d9545678a18bc5
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-09-01 19:51:43 -0500

    feat(visualizations): add GeneralNote model and Note model with mocks for User and Document

--- visualization/models.py
+++ visualization/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 
-# Create your models here.
 
-class Review (models.Model):
-    content = models.TextField(max_length=300)
+# Mocks to test scenarios
+class MockUser(models.Model):
+    """
+    Mocking model to handle dependencies with User model
+    """
+
+
+class MockDocument(models.Model):
+    """
+    Mocking model to handle dependencies with Document model
+    """
+
+
+class GeneralNote(models.Model):
+    content = models.TextField()
     date = models.DateField()
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    user = models.ForeignKey(MockUser, on_delete=models.CASCADE)
+    document = models.ForeignKey(MockDocument, on_delete=models.CASCADE)
+
+
+class Note(models.Model):
+    content = models.TextField()
+    date = models.DateField()
+    is_favorite = models.BooleanField(default=False)
+    page = models.IntegerField()
+    user = models.ForeignKey(MockUser, on_delete=models.CASCADE)
+    document = models.ForeignKey(MockDocument, on_delete=models.CASCADE)
