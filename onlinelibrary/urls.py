9ac740a7e156992f7102975efa096fb6c7832b13
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-09-02 21:04:24 -0500

    feat(social.views): views implemented in social app

--- onlinelibrary/urls.py
+++ onlinelibrary/urls.py
@@ -1,3 +1,5 @@
+from social.views import *
+from django.urls import path, include
 """
 URL configuration for onlinelibrary project.
 
@@ -11,12 +13,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('social/', include('social.urls')),
 ]
--- social/views.py
+++ social/views.py
@@ -1,1 +1,40 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import User, Collection, Activity
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+
+def hello(request):
+    # TODO implement login
+    user = authenticate(request, username="test", password="test")
+    if user is not None:
+        login(request, user)
+    return HttpResponse("Hello World")
+
+
+def profile(request, user_id):
+    user_profile = User.objects.get(id=user_id)
+    return render(request, 'social/profile.html', {'user_profile': user_profile})
+
+
+@login_required
+def feed(request):
+    user_profile = User.objects.get(id=request.user.id)
+    return render(request, 'social/feed.html', {'user_profile': user_profile})
+
+
+@login_required
+def follow_collection(request, collection_id):
+    collection = Collection.objects.get(id=collection_id)
+    user_profile = User.objects.get(id=request.user.id)
+    user_profile.follow(collection)
+    return redirect('collections', user_id=request.user.id)
+
+
+@login_required
+def follow_reader(request, user_id):
+    reader_to_follow = User.objects.get(id=user_id)
+    user_profile = User.objects.get(id=request.user.id)
+    user_profile.follow(reader_to_follow)
+    return redirect('profile', user_id=user_id)
