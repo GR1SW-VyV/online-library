f22a1e419c75f7be64654a2345885c654b7087da
Mateo Díaz <mateo.diaz@epn.edu.ec>
2023-09-07 10:13:25 -0500

    Revert "Merge remote-tracking branch 'origin/team5-feature3' into team5-feature3"
    This reverts commit 736b99aad5339f9e237d3a1f1b0450d62a280d8c, reversing
    changes made to c42a58d341bdd20e0905a13eb3d315099a6dafb8.

---
+++ social/models/collection.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from social.models.activity import CollectionActivity
+from social.models.observable import Observable
+
+
+class Collection(Observable, models.Model):
+    name = models.CharField(max_length=20)
+    followers = models.ManyToManyField('User', symmetrical=False, blank=True, related_name='collection_following')
+
+    def add_document(self, document):
+        document.collections.add(self)
+        activity = self.create_collection_activity(document)
+        activity.save()
+        self.add_activity(activity)
+
+    def add_follower(self, observer):
+        self.followers.add(observer)
+
+    def create_collection_activity(self, document):
+        activity = CollectionActivity()
+        activity.collection = self
+        activity.document = document
+        activity.detail = "add a new document"
+        return activity
+
+    def notify(self):
+        for follower in self.followers.all():
+            follower.update(self.activities[-1])
