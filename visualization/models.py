665f67ac74e6d1e0e21e4d18f2e039ef7a6325e3
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-09-07 10:16:39 -0500

    feat(visualization): add methods to order notes

--- visualization/models.py
+++ visualization/models.py
@@ -1,25 +1,13 @@
 from django.db import models
-
-
-# Mocks to test scenarios
-class MockUser(models.Model):
-    """
-    Mocking model to handle dependencies with User model
-    """
-    username = models.TextField()
-
-
-class MockDocument(models.Model):
-    """
-    Mocking model to handle dependencies with Document model
-    """
+from social.models import User
+from articles.models import Document
 
 
 class GeneralNote(models.Model):
     content = models.TextField()
     date = models.DateField()
-    user = models.ForeignKey(MockUser, on_delete=models.CASCADE)
-    document = models.ForeignKey(MockDocument, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
 
 
 class PageNote(models.Model):
@@ -27,29 +15,48 @@ class PageNote(models.Model):
     date = models.DateField()
     is_favorite = models.BooleanField(default=False)
     page = models.IntegerField()
-    user = models.ForeignKey(MockUser, on_delete=models.CASCADE)
-    document = models.ForeignKey(MockDocument, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
 
 
 class PageNoteDAO:
     @classmethod
-    def get_personal_page_notes(cls, username, book_id, page):
-        notes = PageNote.objects.all().filter(user__username=username, document__id=book_id, page=page)
-        return list(notes)
+    def get_personal_page_notes(cls, username, document_id, page):
+        notes = PageNote.objects.all().filter(user__username=username, document__id=document_id, page=page)
+        ordered_notes = cls.order_notes(notes)
+        return list(ordered_notes)
 
     @classmethod
-    def get_page_notes(cls, username, book_id, page):
-        notes = PageNote.objects.all().exclude(user__username=username).filter(document__id=book_id, page=page)
-        return list(notes)
+    def get_page_notes(cls, username, document_id, page):
+        notes = PageNote.objects.all().exclude(user__username=username).filter(document__id=document_id, page=page)
+        ordered_notes = cls.order_notes(notes)
+        return list(ordered_notes)
+
+    @classmethod
+    def order_notes(cls, notes):
+        ordered_notes = sorted(notes, key=lambda note: note.user.is_professor(), reverse=True)
+        ordered_notes = sorted(ordered_notes, key=lambda note: note.is_favorite, reverse=True)
+        ordered_notes = sorted(ordered_notes, key=lambda note: note.user.followers.count())
+        ordered_notes = sorted(ordered_notes, key=lambda note: note.date)
+        return ordered_notes
 
 
 class GeneralNoteDAO:
     @classmethod
-    def get_personal_general_notes(cls, username, book_id):
-        notes = GeneralNote.objects.all().filter(user__username=username, document__id=book_id)
-        return list(notes)
+    def get_personal_general_notes(cls, username, document_id):
+        notes = GeneralNote.objects.all().filter(user__username=username, document__id=document_id)
+        ordered_notes = cls.order_notes(notes)
+        return list(ordered_notes)
 
     @classmethod
-    def get_general_notes(cls, username, book_id):
-        notes = GeneralNote.objects.all().exclude(user__username=username).filter(document__id=book_id)
-        return list(notes)
+    def get_general_notes(cls, username, document_id):
+        notes = GeneralNote.objects.all().exclude(user__username=username).filter(document__id=document_id)
+        ordered_notes = cls.order_notes(notes)
+        return list(ordered_notes)
+
+    @classmethod
+    def order_notes(cls, notes):
+        ordered_notes = sorted(notes, key=lambda note: note.user.is_professor(), reverse=True)
+        ordered_notes = sorted(ordered_notes, key=lambda note: note.user.followers.count())
+        ordered_notes = sorted(ordered_notes, key=lambda note: note.date)
+        return ordered_notes
