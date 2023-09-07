d7a58cbbc7a92f30fa1f7747abe750a3d5c6971a
dennis chamba <dennis.chamba@epn.edu.ec>
2023-09-07 10:47:55 -0500

    refactor(recommendations): configuration on views.py

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,81 +1,26 @@
 from django.shortcuts import render
+import articles.models
+import bookcollections.models
+from articles import models
+from bookcollections import models
+from social.models import User
+from recommendations.models import RecommendationEngine
+from articles.choices.category import Category
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def view_recommendations(request):
+    user = User.objects.get(id=request.user.id)
+
+    recomender = RecommendationEngine(user)
+    recommendations = recomender.get_recomendations()
     return render(request, './recommendation/recommended.html', context={'recommendations': recommendations})
 
-
+#@login_required
 def view_form_preferences(request):
-    return render(request, './recommendation/form_preferences.html', context={'recommendations': recommendations})
+    print(request.method)
+    return render(request, './recommendation/form_preferences.html',)
 
 
-# create recommendation data for test
-recommendations = [
-    {
-        'category': 'Matemática',
-        'books': [
-            {
-                'title': 'Libro 1',
-                'author': 'Autor 1',
-                'document_id': '1'
-            },
-            {
-                'title': 'Libro 2',
-                'author': 'Autor 2',
-                'document_id': '1'
-            },
-{
-                'title': 'Libro 8',
-                'author': 'Autor 8',
-                'document_id': '1'
-            },
-{
-                'title': 'Libro 9',
-                'author': 'Autor 9',
-                'document_id': '12345'
-            }
-        ]
-    },
-    {
-        'category': 'Física',
-        'books': [
-            {
-                'title': 'Libro 3',
-                'author': 'Autor 3',
-            },
-            {
-                'title': 'Libro 4',
-                'author': 'Autor 4',
-            },
-{
-                'title': 'Libro 10',
-                'author': 'Autor 10',
-            },
-{
-                'title': 'Libro 11',
-                'author': 'Autor 11',
-            },
-        ]
-    },
-    {
-        'category': 'Programación',
-        'books': [
-            {
-                'title': 'Libro 5',
-                'author': 'Autor 5',
-            },
-            {
-                'title': 'Libro 6',
-                'author': 'Autor 6',
-            },
-{
-                'title': 'Libro 12',
-                'author': 'Autor 12',
-            },
-{
-                'title': 'Libro 13',
-                'author': 'Autor 13',
-            },
-        ]
-    }
-]
+
