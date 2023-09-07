a802466d330ea9ff11026e10ebdffb5a0b37443a
KennethUlloa <ulloakenth@gmail.com>
2023-09-06 23:45:29 -0500

    feat(onlinelibrary): implemented base template for UI #23
    Base navbar with empty navigation links and responsive behaviour. Installed font awesome.

--- onlinelibrary/settings.py
+++ onlinelibrary/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social',
+    'annotations',
     'behave_django',
     'polymorphic',
     'articles',
@@ -121,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
