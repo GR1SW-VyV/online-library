58256047765cd01f8eb533b121e924e80d51dc3c
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-09-08 00:43:20 -0500

    fix: fix everything

--- social/models/document.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from social.models.collection import Collection
-
-
-class Document(models.Model):
-    title = models.CharField(max_length=30)
-    collections = models.ManyToManyField(Collection, related_name='books')
