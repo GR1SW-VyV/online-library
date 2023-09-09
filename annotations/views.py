193029098094a1a59029d6c98d5f5e8eafcb9e1a
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2023-09-08 20:26:00 -0500

    feat(annotation): Create notes in the template
    The changes made were:
    - The creation of notes from the view was implemented.
    - The filter was implemented to sort the notes.

--- annotations/views.py
+++ annotations/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 from annotations.models import Note, NoteDAO
@@ -10,17 +10,32 @@ from articles.models import Document
 
 
 # Create your views here.
-def mynotes(request):
-    return render(request, '../templates/annotations/my-notes.html')
-
-
 # @login_required
 def book_user_notes(request, document_id):
     current_user = User.objects.get(id=1)
+
     document = Document.objects.get(uid=document_id)
-    notes = NoteDAO.get_all_notes_of_document(current_user.id, document_id)
+    page_counter = int(request.GET.get('page', 1))
+
+    # Create a note
+
+    if request.method == 'POST':
+        annotation_text = request.POST.get('annotation')
+        Note.objects.create(
+            user_id=current_user.id,
+            document_id=document_id,
+            is_favorite=True,
+            content=annotation_text,
+            page=page_counter
+        )
+        # Redirect, to avoid duplicated notes
+        return redirect(f'/annotations/book/{document_id}/?page={page_counter}')
+
+    notes = NoteDAO.get_notes_by_page(user_id=1, document_id=document_id, page=page_counter).order_by('-date')
+
     return render(request, '../templates/annotations/my-notes.html',
                   {'document': document,
                    'user': current_user,
-                   'notes': notes
+                   'notes': notes,
+                   'page': page_counter
                    })
