1bfcb07514705f0325228ad3a2ff3bb57fb93182
Oscar Cunduri <oscar.cunduri@epn.edu.ec>
2023-09-08 20:23:13 -0500

    delete(annotations): Delete test path

--- annotations/urls.py
+++ annotations/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('my-notes/', views.mynotes),
     path('book/<int:document_id>/', views.book_user_notes, name='book_user_notes'),
 ]
-
