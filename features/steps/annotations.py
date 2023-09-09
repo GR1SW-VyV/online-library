c20f884a723f2ef80a7c7a4a6a1f10de61944085
Betsabe Amaguai <betsabe.amaguai@epn.edu.ec>
2023-09-08 21:51:44 -0500

    feat(create template visualizarInformacion )

---
+++ features/steps/annotations.py
@@ -0,0 +1,80 @@
+from behave import *
+from faker import Faker
+from annotations.testmodels.note import Note as TestNote
+from annotations.testmodels.user import User as TestUser
+from annotations.testmodels.document import Document as TestDocument
+from annotations.testmodels.generalnote import GeneralNote as TestGneralNote
+
+from social.models import User
+from articles.models import Document
+from annotations.models import Note, NoteDAO
+
+use_step_matcher("re")
+
+
+@given('I am reading a document about "Lean Software Development"')
+def step_impl(context):
+    context.document = TestDocument("Lean Software Development", 3)
+    context.user = TestUser(3)
+
+
+@when('I add a note with the text "Tomar en cuenta estos principios" in the page 10')
+def step_impl(context):
+    context.note = TestNote(1, "Tomar en cuenta estos principios", 10, 3, 3)
+    context.document.add_notePage(context.note)
+
+
+@then("I should see the note in the notes section")
+def step_impl(context):
+    assert context.document.notePage_in_document(context.note)
+
+
+@given('I am seeing the document information about "Ensayo sobre la ceguera"')
+def step_impl(context):
+    context.document = TestDocument("Ensayo sobre la ceguera", 2)
+    context.user = TestUser(2)
+
+
+@when('I add an note with the text "Que interesante libro"')
+def step_impl(context):
+    context.generalNote = TestGneralNote("Que interesante libro", 2, 2)
+    context.document.add_generalNote(context.generalNote)
+
+
+@then("I should see the note with the information of the document")
+def step_impl(context):
+    assert context.document.note_in_document(context.generalNote)
+
+
+@given('I am reading the book "Cien años de soledad"')
+def step_impl(context):
+
+    context.document = Document.objects.create(
+        title="Cien años de soledad"
+    )
+
+    faker = Faker()
+    context.user = User.objects.create(
+        username=faker.user_name(),
+    )
+
+
+@step("I want to take an important note")
+def step_impl(context):
+    context.note = Note.objects.create(
+        content="Me parece muy importante la línea 23",
+        page=23,
+        user=context.user,
+        document=context.document,
+    )
+
+
+@when("I mark the note as favorite")
+def step_impl(context):
+    NoteDAO.mark_note_as_favorite(context.note.id)
+
+
+@then("I should see the note with the mark")
+def step_impl(context):
+    note = NoteDAO.get_note_by_id(context.note.id)
+    assert note.is_favorite is True
---
+++ static/app.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const pdfViewer = document.getElementById("pdfViewer");
+    const currentPageSpan = document.getElementById("currentPage");
+    const totalPagesSpan = document.getElementById("totalPages");
+    let currentPage = 1; // Inicialmente en la página 1
+    let totalPages = 1;
+
+    pdfjsLib
+        .getDocument(pdfUrl)
+        .promise.then(function (pdfDoc) {
+        // Carga la primera página del PDF
+        totalPages = pdfDoc.numPages;
+        totalPagesSpan.textContent = totalPages;
+        return pdfDoc.getPage(1);
+    })
+        .then(function (page) {
+
+            const scale = 1.0;
+            const viewport = page.getViewport({scale: scale});
+
+            // Crea un elemento canvas para mostrar la página
+            const canvas = document.createElement("canvas");
+            const context = canvas.getContext("2d");
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Agrega el canvas al visor de PDF
+            pdfViewer.innerHTML = ""; // Limpia el contenedor si hay contenido previo
+            pdfViewer.appendChild(canvas);
+
+            // Muestra la página en el canvas
+            page.render({
+                canvasContext: context,
+                viewport: viewport,
+            });
+
+            // OJO: Actualiza el número de página actual del documento PDF
+            currentPage = 1;
+            updatePage()
+
+           // currentPageSpan.textContent = currentPage;
+        });
+
+
+    // Función para cambiar a la página siguiente
+    function nextPage() {
+        currentPage += 1;
+        updatePage();
+    }
+
+    // Función para cambiar a la página anterior
+    function prevPage() {
+        currentPage -= 1;
+        updatePage();
+    }
+
+    // Actualiza el número de página y muestra la página correspondiente
+    function updatePage() {
+        if (pdfViewer.firstChild) {
+            pdfViewer.firstChild.remove(); // Elimina el canvas actual
+        }
+
+        pdfjsLib
+            .getDocument(pdfUrl)
+            .promise.then(function (pdfDoc) {
+            if (currentPage < 1) {
+                currentPage = 1;
+            } else if (currentPage > pdfDoc.numPages) {
+                currentPage = pdfDoc.numPages;
+            }
+
+            currentPageSpan.textContent = currentPage;
+
+            totalPages = pdfDoc.numPages;
+            totalPagesSpan.textContent = totalPages;
+
+            return pdfDoc.getPage(currentPage);
+        })
+            .then(function (page) {
+                const scale = 1.0;
+                const viewport = page.getViewport({scale: scale});
+
+                const canvas = document.createElement("canvas");
+                const context = canvas.getContext("2d");
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                pdfViewer.appendChild(canvas);
+
+                page.render({
+                    canvasContext: context,
+                    viewport: viewport,
+                });
+            });
+    }
+
+    // Escucha eventos de los botones para cambiar de página
+    const nextPageButton = document.getElementById("nextPage");
+    nextPageButton.addEventListener("click", nextPage);
+
+    const prevPageButton = document.getElementById("prevPage");
+    prevPageButton.addEventListener("click", prevPage);
+
+});
