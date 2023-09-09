66d1ece44cb2a1e9db048698b4e00c68cf98f42f
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-09-09 10:08:37 -0500

    feat(visualization): order by date

--- visualization/models.py
+++ visualization/models.py
@@ -25,15 +25,23 @@ class PageNoteDAO:
     def get_personal_page_notes(cls, username, book_id, page):
         notes = PageNote.objects.all().filter(user__username=username, document__uid=book_id, page=page)
         ordered_notes = list(notes)
-        ordered_notes.sort(key=lambda note: (-note.is_favorite, note.date))
+        ordered_notes.sort(key=lambda note: (-note.is_favorite, -note.date))
         return list(ordered_notes)
 
     @classmethod
     def get_page_notes(cls, username, book_id, page):
         notes = PageNote.objects.all().exclude(user__username=username).filter(document__uid=book_id, page=page)
         ordered_notes = list(notes)
-        ordered_notes.sort(key=lambda note: (-note.user.is_professor(), -note.is_favorite, note.user.followers, note.date))
+        ordered_notes.sort(
+            key=lambda note: (-note.user.is_professor(), -note.is_favorite, note.user.followers, -note.date))
         return list(ordered_notes)
+
+    @classmethod
+    def get_str_page_notes(cls, page_notes):
+        notes = ""
+        for note in page_notes:
+            notes += note.content + ","
+        return notes[:-1]
 
 
 class GeneralNoteDAO:
@@ -41,12 +49,19 @@ class GeneralNoteDAO:
     def get_personal_general_notes(cls, username, book_id):
         notes = GeneralNote.objects.all().filter(user__username=username, document__uid=book_id)
         ordered_notes = list(notes)
-        ordered_notes.sort(key=lambda note: note.date)
+        ordered_notes.sort(key=lambda note: note.date, reverse=True)
         return list(ordered_notes)
 
     @classmethod
     def get_general_notes(cls, username, book_id):
         notes = GeneralNote.objects.all().exclude(user__username=username).filter(document__uid=book_id)
         ordered_notes = list(notes)
-        ordered_notes.sort(key=lambda note: (-note.user.is_professor(), note.user.followers, note.date))
+        ordered_notes.sort(key=lambda note: (-note.user.is_professor(), note.user.followers, -note.date))
         return list(ordered_notes)
+
+    @classmethod
+    def get_str_general_notes(cls, general_notes):
+        notes = ""
+        for note in general_notes:
+            notes += note.content + ","
+        return notes[:-1]
