0c64f0d6b0349fa393190129e592f25aaf56497c
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-09-09 01:20:02 -0500

    feat(visualization): add logic to order notes

--- visualization/models.py
+++ visualization/models.py
@@ -24,21 +24,29 @@ class PageNoteDAO:
     @classmethod
     def get_personal_page_notes(cls, username, book_id, page):
         notes = PageNote.objects.all().filter(user__username=username, document__id=book_id, page=page)
-        return list(notes)
+        ordered_notes = list(notes)
+        ordered_notes.sort(key=lambda note: (-note.is_favorite, note.date))
+        return list(ordered_notes)
 
     @classmethod
     def get_page_notes(cls, username, book_id, page):
         notes = PageNote.objects.all().exclude(user__username=username).filter(document__id=book_id, page=page)
-        return list(notes)
+        ordered_notes = list(notes)
+        ordered_notes.sort(key=lambda note: (-note.user.is_professor(), -note.is_favorite, note.user.followers, note.date))
+        return list(ordered_notes)
 
 
 class GeneralNoteDAO:
     @classmethod
     def get_personal_general_notes(cls, username, book_id):
         notes = GeneralNote.objects.all().filter(user__username=username, document__id=book_id)
-        return list(notes)
+        ordered_notes = list(notes)
+        ordered_notes.sort(key=lambda note: note.date)
+        return list(ordered_notes)
 
     @classmethod
     def get_general_notes(cls, username, book_id):
         notes = GeneralNote.objects.all().exclude(user__username=username).filter(document__id=book_id)
-        return list(notes)
+        ordered_notes = list(notes)
+        ordered_notes.sort(key=lambda note: (-note.user.is_professor(), note.user.followers, note.date))
+        return list(ordered_notes)
