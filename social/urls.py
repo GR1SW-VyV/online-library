a24a764b4b44b5129068f2a065b1c5b76df86f1f
Nicolás Toscano <nicolas.toscano@epn.edu.ec>
2023-09-10 17:22:34 -0500

    fix(social): passing the username in views.py
    Related to #88

--- social/urls.py
+++ social/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<int:user_id>', views.profile, name='profile'),
+    path('profile/<str:user_username>', views.profile, name='profile'),
     path('feed', views.feed, name='feed'),
     path('follow_collection/<int:collection_id>', views.follow_collection, name='follow_collection'),
-    path('follow_reader/<int:user_id>', views.follow_reader, name='follow_reader'),
+    path('follow_reader/<str:user_username>', views.follow_reader, name='follow_reader'),
 ]
