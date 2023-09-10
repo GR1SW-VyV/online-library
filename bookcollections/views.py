6a4d2e29881feda7b4de9a13a80382ad1c828dfe
KennethUlloa <ulloakenth@gmail.com>
2023-09-10 12:24:41 -0500

    refactor(bookcollections): add breadcrums and fixed navbar

--- bookcollections/views.py
+++ bookcollections/views.py
@@ -38,15 +38,21 @@ def view_collections(request):
     collections = []
     search = request.GET.get("search", "")
     tipo = request.GET.get("tipo", "")
-    is_partial = False
+    privacidad = request.GET.get("privacy", "all")
+
     if search and tipo:
-        is_partial = True
+
         if tipo == "name":
             collections = CollectionDAO.search_by_name_and_user(search, request.user.id)
         elif tipo == "category":
             collections = CollectionDAO.search_by_category_and_user(search, request.user.id)
     else:
         collections = CollectionDAO.get_all_by_user(request.user.id)
+
+    if privacidad == "public":
+        collections = collections.filter(is_public=True)
+    elif privacidad == "private":
+        collections = collections.filter(is_public=False)
 
     return render(request, './collections/collections.html', context={'collections': collections, 'search': search})
 
@@ -69,7 +75,7 @@ def create_coll(request):
     elif request.POST:
         name = request.POST["name"]
         desc = request.POST["description"]
-        is_public= request.POST["is_private"] == "on"
+        is_public = request.POST.get("is_public", "") == "on"
         category = request.POST["category"]
         book = request.POST["doc_id"]
 
@@ -84,56 +90,3 @@ def create_coll(request):
 
         return redirect('/collections')
     return render(request, './collections/create.html')
-
-
-collections2 = [
-    {
-        'id': 1,
-        'name': 'collection1',
-        'cover': 'https://i.pinimg.com/564x/91/83/7d/91837dd303b431b003d9d9e76ea91d30.jpg',
-        'description': 'description1',
-        'categories': ['Categoría A', 'Categoría B'],
-        'books': [
-            {
-                'title': 'Libro 1',
-                'author': 'Autor 1',
-
-            },
-            {
-                'title': 'Libro 2',
-                'author': 'Autor 2',
-            },
-        ]
-    },
-    {
-        'id': 2,
-        'name': 'collection2',
-        'description': 'description2',
-        'cover': 'https://i.pinimg.com/564x/d1/a5/cc/d1a5cc99e240591db113023438329fce.jpg',
-        'categories': ['Categoría C', 'Categoría D'],
-        'books': [
-            {
-                'title': 'Libro 1',
-                'author': 'Autor 1',
-
-            },
-        ]
-    },
-    {
-        'id': 3,
-        'name': 'collection3',
-        'cover': 'https://i.pinimg.com/564x/a6/46/b2/a646b21d519edd807e7817e14c28af07.jpg',
-        'description': 'description3',
-        'categories': ['Categoría F', 'Categoría E'],
-        'books': [
-            {
-                'title': 'Libro 1',
-                'author': 'Autor 1',
-            },
-            {
-                'title': 'Libro 7',
-                'author': 'Autor 7',
-            },
-        ]
-    },
-]
