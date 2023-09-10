1f927b509bbb13c2c71a819acd5b3fc26108e1a7
Bryan Andrade <alexanderandrade252@gmail.com>
2023-09-10 12:46:07 -0500

    refactor: all scenarios and the steps created

--- features/annotations.feature
+++ features/annotations.feature
@@ -6,23 +6,36 @@ Feature: Write notes
   Scenario Template: Take notes of a page of a document
     Given I am reading a document <document_title>
     When I add a note with the text <text> in the page <page_number>
-    Then I should see the note in the notes section of the page <page_number_with_note>
+    Then I should see the note in the notes section of the page <page_number>
 
     Examples:
-      |      document_title      | text                                |  page_number | page_number_with_note |
-      | Lean Software            | Tomar en cuenta estos principios    | 1            | 1                     |
-      | Math Book                | Estudiar esto                       | 0            | 0                     |
-      | Physics Book             |                                     | 1000         | 1000                  |
+      |      document_title      | text                                |  page_number |
+      | Lean Software            | Tomar en cuenta estos principios    | 1            |
+      | Math Book                | Estudiar esto                       | 0            |
+      | Physics Book             |                                     | 1000         |
 
-  Scenario: Take notes of a document
-    Given I am seeing the document information about "Ensayo sobre la ceguera"
-    When I add an note with the text "Que interesante libro"
-    Then I should see the note with the information of the document
+  Scenario Template: Take notes of a document
+    Given I am seeing the document information about a document <document_title>
+    When I add an note with the text <text>
+    Then I should see the note with the information of the document <document_title>
+
+    Examples:
+      |      document_title      | text                                |
+      | Lean Software            | Tomar en cuenta estos principios    |
+      | Math Book                | Estudiar esto                       |
+      | Physics Book             |                                     |
 
 
 
-  Scenario: Mark an annotation as favorite
-    Given I am reading the book "Cien años de soledad"
-    And I want to take an important note
-    When I mark the note as favorite
-    Then I should see the note with the mark
+
+  Scenario Template: Mark an annotation as favorite
+    Given I am reading the book <document_title>
+    And I want to take an important note <text>
+    When I mark the note as favorite <favorite>
+    Then I should see the note with the mark <favorite>
+
+    Examples:
+      |      document_title      | text                                |  favorite   |
+      | Lean Software            | Tomar en cuenta estos principios    | True        |
+      | Math Book                | Estudiar esto                       | False       |
+      | Physics Book             |                                     | True        |
--- features/steps/annotations.py
+++ features/steps/annotations.py
@@ -1,68 +1,6 @@
 from behave import *
-from faker import Faker
-from annotations.testmodels.note import Note as TestNote
-from annotations.testmodels.user import User as TestUser
-from annotations.testmodels.document import Document as TestDocument
-from annotations.testmodels.generalnote import GeneralNote as TestGneralNote
-
-from social.models import User
-from articles.models import Document
-from annotations.models import PageNote, PageNoteDAO
 
 use_step_matcher("re")
-
-
-
-@given('I am seeing the document information about "Ensayo sobre la ceguera"')
-def step_impl(context):
-    context.document = TestDocument("Ensayo sobre la ceguera", 2)
-    context.user = TestUser(2)
-
-
-@when('I add an note with the text "Que interesante libro"')
-def step_impl(context):
-    context.generalNote = TestGneralNote("Que interesante libro", 2, 2)
-    context.document.add_generalNote(context.generalNote)
-
-
-@then("I should see the note with the information of the document")
-def step_impl(context):
-    assert context.document.note_in_document(context.generalNote)
-
-
-@given('I am reading the book "Cien años de soledad"')
-def step_impl(context):
-
-    context.document = Document.objects.create(
-        title="Cien años de soledad"
-    )
-
-    faker = Faker()
-    context.user = User.objects.create(
-        username=faker.user_name(),
-        password=faker.password()
-    )
-
-
-@step("I want to take an important note")
-def step_impl(context):
-    context.note = PageNote.objects.create(
-        content="",
-        page=23,
-        user=context.user,
-        document=context.document,
-    )
-
-
-@when("I mark the note as favorite")
-def step_impl(context):
-    PageNoteDAO.mark_note_as_favorite(context.note.id)
-
-
-@then("I should see the note with the mark")
-def step_impl(context):
-    note = PageNoteDAO.get_note_by_id(context.note.id)
-    assert note.is_favorite is True
 
 
 @given("I am reading a document (?P<document_title>.+)")
@@ -75,7 +13,41 @@ def step_impl(context, text, page_number):
     raise NotImplementedError(u'STEP: When I add a note with the text <text> in the page <page_number>')
 
 
-@then("I should see the note in the notes section of the page (?P<page_number_with_note>.+)")
-def step_impl(context, page_number_with_note):
-    raise NotImplementedError(
-        u'STEP: Then I should see the note in the notes section of the page <page_number_with_note>')
+@then("I should see the note in the notes section of the page (?P<page_number>.+)")
+def step_impl(context, page_number):
+    raise NotImplementedError(u'STEP: Then I should see the note in the notes section of the page <page_number>')
+
+
+@given("I am seeing the document information about a document (?P<document_title>.+)")
+def step_impl(context, document_title):
+    raise NotImplementedError(u'STEP: Given I am seeing the document information about a document <document_title>')
+
+
+@when("I add an note with the text (?P<text>.+)")
+def step_impl(context, text):
+    raise NotImplementedError(u'STEP: When I add an note with the text <text>')
+
+
+@then("I should see the note with the information of the document (?P<document_title>.+)")
+def step_impl(context, document_title):
+    raise NotImplementedError(u'STEP: Then I should see the note with the information of the document <document_title>')
+
+
+@given("I am reading the book (?P<document_title>.+)")
+def step_impl(context, document_title):
+    raise NotImplementedError(u'STEP: Given I am reading the book <document_title>')
+
+
+@step("I want to take an important note (?P<text>.+)")
+def step_impl(context, text):
+    raise NotImplementedError(u'STEP: And I want to take an important note <text>')
+
+
+@when("I mark the note as favorite (?P<favorite>.+)")
+def step_impl(context, favorite):
+    raise NotImplementedError(u'STEP: When I mark the note as favorite <favorite>')
+
+
+@then("I should see the note with the mark (?P<favorite>.+)")
+def step_impl(context, favorite):
+    raise NotImplementedError(u'STEP: Then I should see the note with the mark <favorite>')
