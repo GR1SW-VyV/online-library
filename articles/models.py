0ee898780f436547e2c43871b71291a4b41639b6
LuisM45 <luismateoarg2012@gmail.com>
2023-09-10 22:33:24 -0500

    fix: js chrome call

--- articles/models.py
+++ articles/models.py
@@ -71,6 +71,7 @@ class Document(models.Model):
         return f"/static/articles/resources/{category_str}Resources/{self.sha512[:32]}/{self.filename}"
 
     def add_score(self, user_id, score):
+        print(user_id)
         old_score = Score.objects.filter(user=user_id,document=self).first()
         if old_score is None:
             Score(user=user_id,document=self,value=score).save()
